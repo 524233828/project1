84c9bede2c9fe235e9d4ac4aa6e16e600418ffad
JoseChan <chenyu01@linghit.com>
2017-11-23 16:43:51 +0800

    fix

--- src/Logic/UploadLogic.php
+++ src/Logic/UploadLogic.php
@@ -22,7 +22,7 @@ class UploadLogic extends BaseLogic
             "image/bmp"
         ]);
 
-        $path = app()->get("config")->get("upload_dir")."/".app()->get("config")->get("img_dir")."/".$file->getName().".".$file->getExtension();
+        $path = app()->get("config")->get("upload_dir")."/".$file->getName().".".$file->getExtension();
 
         $data = [
             "resource_id" => $file->getName(),
@@ -41,7 +41,7 @@ class UploadLogic extends BaseLogic
     {
         $file = Uploader::save($name,["video/mpeg","video/mp4"]);
 
-        $path = app()->get("config")->get("upload_dir")."/".app()->get("config")->get("video_dir")."/".$file->getName().".".$file->getExtension();
+        $path = app()->get("config")->get("upload_dir")."/".$file->getName().".".$file->getExtension();
 
         $data = [
             "resource_id" => $file->getName(),
