2c11ceb8d62bcad46ab998d0f1d90a94dd103791
JoseChan <chenyu01@linghit.com>
2017-11-29 19:05:14 +0800

    fux

--- src/Controller/AdminUserTestController.php
+++ src/Controller/AdminUserTestController.php
@@ -18,13 +18,16 @@ class AdminUserTestController extends BaseController
     /**
      * @name 后台获取参加测试的用户列表
      * @apiParam page|int|页数|false
-     * @returnParam [].id|int|记录ID
-     * @returnParam [].user_id|int|用户ID
-     * @returnParam [].nickname|string|用户名
-     * @returnParam [].test_id|int|测试ID
-     * @returnParam [].title|string|测试名
-     * @returnParam [].channel_name|string|渠道号
-     * @returnParam [].create_time|int|测试时间戳
+     * @returnParam list[].id|int|记录ID
+     * @returnParam list[].user_id|int|用户ID
+     * @returnParam list[].nickname|string|用户名
+     * @returnParam list[].test_id|int|测试ID
+     * @returnParam list[].title|string|测试名
+     * @returnParam list[].channel_name|string|渠道号
+     * @returnParam list[].create_time|int|测试时间戳
+     * @returnParam total_page|int|总页数
+     * @returnParam total_count|int|总条数
+     * @returnParam row_num|int|一页多少行
      * @param ServerRequest $request
      * @return \Service\ApiResponse
      */
