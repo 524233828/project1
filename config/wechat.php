52b01a143a529606611ff4c0fcfdf07944e3d868
JoseChan <chenyu01@linghit.com>
2017-12-06 15:21:04 +0800

    fix

--- config/wechat.php
+++ config/wechat.php
@@ -45,7 +45,7 @@ return [
         'key'                => 'yaoyuan123*',
 //        'cert_path'          => 'path/to/your/cert.pem', // XXX: 绝对路径！！！！
 //        'key_path'           => 'path/to/your/key',      // XXX: 绝对路径！！！！
-        'notify_url'         => '/common/order_notify',      // XXX: 绝对路径！！！！
+        'notify_url'         => 'http://www.ym8800.com/common/order_notify',      // XXX: 绝对路径！！！！
         // 'device_info'     => '013467007045764',
         // 'sub_app_id'      => '',
         // 'sub_merchant_id' => '',
