79055062c14cc89c6687a9b501d079809083f120
JoseChan <chenyu01@linghit.com>
2018-09-21 12:13:05 +0800

    fix

--- config/wechat.php
+++ config/wechat.php
@@ -10,8 +10,8 @@ return [
     /**
      * 账号基本信息，请从微信公众平台/开放平台获取
      */
-    'app_id'  => 'wx85ba94e795ed698e',         // AppID
-    'secret'  => '57a6d4c30b655ff90708478fec40f929',     // AppSecret
+    'app_id'  => 'wx38dd89afcc42c109',         // AppID
+    'secret'  => 'f354f76c3c06fa8ba45e47ded09dd677',     // AppSecret
     //'token'   => 'your-token',          // Token
 //    'aes_key' => '',                    // EncodingAESKey，安全模式下请一定要填写！！！
     /**
